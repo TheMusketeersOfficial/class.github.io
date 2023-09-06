f17edca93b400de3c0929195d89a85cbe7d6ccc6
PepsiManUnblocked <doomsdayrisenow@gmail.com>
2023-09-06 13:26:07 +0000

    begin

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -37,14 +37,34 @@ $(document).ready(function () {
   // ALL OF YOUR CODE SHOULD GO BELOW HERE ////////
   /////////////////////////////////////////////////
 
+ // TODO 0: complete
+
+
   // TODO 1: create a new shape object and add it to the array
+  var shape = {
+    color: "blue",
+    shape: "circle",
+    repeat: 3
+  }
   
+  dataShapes.push(shape);
 
   // TODO 2: add a new property to all data shapes
-  
+  for(var i = 0; i < dataShapes.length; i++){
+    var currentShape = dataShapes[1];
+    if(currentShape.color === "red"){
+      currentShape.goodBehavior = "bounce"
+    }
+    if(currentShape.color === "blue"){
+      currentShape.goodBehavior = "blink"
+    }
+    else if(currentShape.color){
+      currentShape.goodBehavior = "spin"
+    }
+  }
 
   // TODO 3-a: add a function that handles the static display type
-  
+  function handleStatic(data) {}
 
   // TODO 4-a: add a function that handles the good display type
   
