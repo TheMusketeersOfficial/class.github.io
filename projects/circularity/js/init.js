14a2698fa4752f33a7d5c9b6c4c969362ab6921b
PepsiManUnblocked <doomsdayrisenow@gmail.com>
2023-10-04 13:05:11 +0000

    circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -7,7 +7,7 @@ var init = function (window) {
         app = window.opspark.makeApp(),
         canvas = app.canvas, 
         view = app.view,
-        fps = draw.fps('#000');
+        fps = draw.fps('lime');
         
     
     window.opspark.makeGame = function() {
@@ -20,13 +20,29 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
+        var circle;
 
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, false, false, "#00FF00", 2);
+            physikz.addRandomVelocity(circle, canvas, 10, 10);
+            view.addChild(circle);
+            circles.push(circle);
+
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        
+        for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++) {
+            drawCircle();
+          }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,13 +55,17 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
+            
 
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+            
 
             // TODO 9 : Iterate over the array
-           
+            for (var i = 0; i < circles.length; i++) {
+                physikz.updatePosition(circles[i])
+                game.checkCirclePosition(circles[i])
+              }
             
         }
     
@@ -62,8 +82,14 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if ( circle.y > canvas.height ) {
+                circle.y = 0;
+            }
 
+            if ( circle.y < 0 ) {
+                circle.y = canvas.height;
+            }
+            
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
